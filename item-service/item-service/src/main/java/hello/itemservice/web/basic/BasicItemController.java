e79aaae2c24d00f1651848680368edcc24f56bdd
unknown <qkdkran123@gmail.com>
2023-05-04 16:32:58 +0900

    cafe

--- item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -58,11 +55,70 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+   // @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+//@ModelAttribute는 2가지를 같이 처리(모델 객체를 만들어주고, 자동으로 뷰에 넣어주는 역할까지)
+//위의 model 파라미터도 생략가능.
+        itemRepository.save(item);
+//        model.addAttribute("item",item); 자동추가로 생략가능.
+
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        //name을 생략하면
+        //첫 글자를 소문자로 바꾸고 만들어준다.
+        //HelloData면 helloData, Item 이면 item
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    //@PostMapping("/add")
+    public String addItemV4(Item item) {
+        //우리가 만든 클래스면 modelattribute 생략가능,
+        //primitive 타입은 어노테이션을 생략하면 RequestParam이 붙는다.
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId,item);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    //PRG 패턴, Post redirect Get -> Post add가 새로고침 되엇을 때 여러번 호출되는 것을 막을 수 있다.
     @PostMapping("/add")
-    public String save() {
-
-
-        return "basic/addForm";
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/"+item.getId();
     }
 
 
